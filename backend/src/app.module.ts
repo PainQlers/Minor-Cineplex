f37fbdec90bbbc729c1fad5cad97b570b3976a50
Patchara <sombrousnorn@gmail.com>
2026-04-07 01:43:16 +0700

    chore: i nestjs config

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,9 +4,15 @@ import { AppService } from './app.service';
 import { SupabaseService } from './libs/supabase/supabase.service';
 import { MoviesController } from './modules/movies/movies.controller';
 import { AuthModule } from './modules/auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    AuthModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController, MoviesController],
   providers: [AppService, SupabaseService],
 })
